341ff6e10256f6ad0cc54f13d320255b2edd2068
Richard Hughes <richard@hughsie.com>
2021-01-07 16:52:56 +0000

    f

--- contrib/oss-fuzz.sh
+++ contrib/oss-fuzz.sh
@@ -3,9 +3,6 @@
 #
 # SPDX-License-Identifier: LGPL-2.1+
 
-# ensure up to date : FIXME remove
-git pull
-
 if [[ ! -v WORK ]]; then
 	echo "WORK unset, perhaps try WORK=/home/hughsie/Code/fwupd/build-oss-fuzz"
 	exit 1
