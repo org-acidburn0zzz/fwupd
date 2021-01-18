08287554227575d4f5ba4495b48cfa7607e36d8b
Richard Hughes <richard@hughsie.com>
2021-01-18 21:13:28 +0000

    f

--- contrib/ci/oss-fuzz/build.sh
+++ contrib/ci/oss-fuzz/build.sh
@@ -25,10 +25,17 @@ if [ -z "${OUT:-}" ]; then
 fi
 if [ -z "${SRC:-}" ]; then
 	SRC=`realpath .`
+	SRC_FWUPD="${SRC}"
+	SRC_LIBXMLB="${SRC}/libxmlb"
+	SRC_JSONGLIB="${SRC}/json-glib"
+else
+	SRC_FWUPD="${SRC}/fwupd"
+	SRC_LIBXMLB="${SRC}/libxmlb"
+	SRC_JSONGLIB="${SRC}/json-glib"
 fi
 
 # build bits of xmlb
-if [ ! -d "${SRC}/libxmlb" ]; then
+if [ ! -d ${SRC_LIBXMLB} ]; then
 	pushd ${SRC}
 	git clone https://github.com/hughsie/libxmlb.git
 	cd libxmlb
@@ -38,14 +45,14 @@ if [ ! -d "${SRC}/libxmlb" ]; then
 fi
 
 # build bits of json-glib
-if [ ! -d "${SRC}/json-glib" ]; then
+if [ ! -d "${SRC_JSONGLIB}" ]; then
 	pushd ${SRC}
 	git clone https://gitlab.gnome.org/GNOME/json-glib.git
 	popd
 fi
 
 # set up shared / static
-CFLAGS="$CFLAGS -I${SRC}/contrib/ci/oss-fuzz"
+CFLAGS="$CFLAGS -I${SRC_FWUPD}/contrib/ci/oss-fuzz"
 CFLAGS="$CFLAGS -Wno-deprecated-declarations"
 PREDEPS_LDFLAGS="-Wl,-Bdynamic -ldl -lm -lc -pthread -lrt -lpthread"
 DEPS="gmodule-2.0 glib-2.0 gio-unix-2.0 gobject-2.0"
@@ -61,7 +68,7 @@ BUILT_OBJECTS=""
 export PKG_CONFIG="`which pkg-config` --static"
 
 # json-glib
-pushd ${SRC}/json-glib
+pushd ${SRC_JSONGLIB}
 meson \
     --prefix=${WORK} \
     --libdir=lib \
@@ -76,7 +83,7 @@ CFLAGS="$CFLAGS -I${WORK}/include/json-glib-1.0/json-glib -I${WORK}/include/json
 BUILD_LDFLAGS="${BUILD_LDFLAGS} ${WORK}/lib/libjson-glib-1.0.a"
 
 # libxmlb
-pushd ${SRC}/libxmlb
+pushd ${SRC_LIBXMLB}
 meson \
     --prefix=${WORK} \
     --libdir=lib \
@@ -92,7 +99,7 @@ CFLAGS="$CFLAGS -I${WORK}/include/libxmlb-2 -I${WORK}/include/libxmlb-2/libxmlb"
 BUILD_LDFLAGS="${BUILD_LDFLAGS} ${WORK}/lib/libxmlb.a"
 
 # libfwupd shared built objects
-CFLAGS="$CFLAGS -I${SRC} -I${SRC}/libfwupd -I${SRC}/libfwupdplugin "
+CFLAGS="$CFLAGS -I${SRC_FWUPD} -I${SRC_FWUPD}/libfwupd -I${SRC_FWUPD}/libfwupdplugin "
 libfwupd_srcs="\
 	fwupd-common \
 	fwupd-device \
@@ -101,7 +108,7 @@ libfwupd_srcs="\
 	fwupd-release \
 "
 for obj in $libfwupd_srcs; do
-	$CC $CFLAGS ${BUILD_CFLAGS} -c ${SRC}/libfwupd/$obj.c -o ${WORK}/$obj.o
+	$CC $CFLAGS ${BUILD_CFLAGS} -c ${SRC_FWUPD}/libfwupd/$obj.c -o ${WORK}/$obj.o
 	BUILT_OBJECTS="${BUILT_OBJECTS} ${WORK}/$obj.o"
 done
 
@@ -117,13 +124,13 @@ libfwupdplugin_srcs="\
 	fu-volume \
 "
 for obj in $libfwupdplugin_srcs; do
-	$CC $CFLAGS ${BUILD_CFLAGS} -c ${SRC}/libfwupdplugin/$obj.c -o ${WORK}/$obj.o
+	$CC $CFLAGS ${BUILD_CFLAGS} -c ${SRC_FWUPD}/libfwupdplugin/$obj.c -o ${WORK}/$obj.o
 	BUILT_OBJECTS="${BUILT_OBJECTS} ${WORK}/$obj.o"
 done
 
 # dummy binary entrypoint
 if [ -z "${LIB_FUZZING_ENGINE:-}" ]; then
-	$CC $CFLAGS ${BUILD_CFLAGS} -c ${SRC}/libfwupdplugin/fu-fuzzer-main.c -o ${WORK}/fu-fuzzer-main.o
+	$CC $CFLAGS ${BUILD_CFLAGS} -c ${SRC_FWUPD}/libfwupdplugin/fu-fuzzer-main.c -o ${WORK}/fu-fuzzer-main.o
 	BUILT_OBJECTS="${BUILT_OBJECTS} ${WORK}/fu-fuzzer-main.o"
 else
 	BUILT_OBJECTS="${BUILT_OBJECTS} ${LIB_FUZZING_ENGINE}"
@@ -135,10 +142,10 @@ BUILD_CFLAGS_FUZZER_FIRMWARE="-Wno-implicit-function-declaration -Wno-int-conver
 # DFU
 fuzzer_type="dfu"
 fuzzer_name="fu-${fuzzer_type}-firmware"
-$CC $CFLAGS ${BUILD_CFLAGS} -c ${SRC}/libfwupdplugin/fu-${fuzzer_type}-firmware.c -o ${WORK}/$fuzzer_name.o
+$CC $CFLAGS ${BUILD_CFLAGS} -c ${SRC_FWUPD}/libfwupdplugin/fu-${fuzzer_type}-firmware.c -o ${WORK}/$fuzzer_name.o
 $CC $CFLAGS ${BUILD_CFLAGS} ${BUILD_CFLAGS_FUZZER_FIRMWARE} \
 	-DGOBJECTTYPE=fu_${fuzzer_type}_firmware_new -c \
-	${SRC}/libfwupdplugin/fu-fuzzer-firmware.c -o ${WORK}/${fuzzer_name}_fuzzer.o
+	${SRC_FWUPD}/libfwupdplugin/fu-fuzzer-firmware.c -o ${WORK}/${fuzzer_name}_fuzzer.o
 $CXX $CXXFLAGS ${BUILT_OBJECTS} ${WORK}/$fuzzer_name.o ${WORK}/${fuzzer_name}_fuzzer.o \
 	-o $OUT/${fuzzer_name}_fuzzer ${BUILD_LDFLAGS}
-zip --junk-paths $OUT/${fuzzer_name}_fuzzer_seed_corpus.zip ${SRC}/src/fuzzing/firmware/${fuzzer_type}*
+zip --junk-paths $OUT/${fuzzer_name}_fuzzer_seed_corpus.zip ${SRC_FWUPD}/src/fuzzing/firmware/${fuzzer_type}*
